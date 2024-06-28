25a5b4a51337827f10af2c19222dc458660667f4
magneum-II <yt-dlx@proton.me>
2024-06-29 00:28:38 +0530

    updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,12 +6,12 @@ import { Roboto } from "next/font/google";
 import LocalFontLoader from "next/font/local";
 import AnimatedContentWrapper from "@/app/utils/components/AnimatedContentWrapper";
 
-const Brittany_Signature = LocalFontLoader({ variable: "--font-Brittany_Signature", src: "../pages/fonts/Brittany_Signature.otf" });
-const Helvetica_Rounded = LocalFontLoader({ variable: "--font-Helvetica_Rounded", src: "../pages/fonts/Helvetica_Rounded.otf" });
-const Hatton_Bold = LocalFontLoader({ variable: "--font-Hatton_Bold", src: "../pages/fonts/Hatton_Bold.otf" });
-const MaronRose = LocalFontLoader({ variable: "--font-MaronRose", src: "../pages/fonts/MaronRose.ttf" });
-const Helvetica = LocalFontLoader({ variable: "--font-Helvetica", src: "../pages/fonts/Helvetica.ttf" });
-const Grenoble = LocalFontLoader({ variable: "--font-Grenoble", src: "../pages/fonts/Grenoble.ttf" });
+const Brittany_Signature = LocalFontLoader({ variable: "--font-Brittany_Signature", src: "./fonts/Brittany_Signature.otf" });
+const Helvetica_Rounded = LocalFontLoader({ variable: "--font-Helvetica_Rounded", src: "./fonts/Helvetica_Rounded.otf" });
+const Hatton_Bold = LocalFontLoader({ variable: "--font-Hatton_Bold", src: "./fonts/Hatton_Bold.otf" });
+const MaronRose = LocalFontLoader({ variable: "--font-MaronRose", src: "./fonts/MaronRose.ttf" });
+const Helvetica = LocalFontLoader({ variable: "--font-Helvetica", src: "./fonts/Helvetica.ttf" });
+const Grenoble = LocalFontLoader({ variable: "--font-Grenoble", src: "./fonts/Grenoble.ttf" });
 
 const roboto = Roboto({ style: ["normal", "italic"], weight: ["400", "700"], subsets: ["latin"], display: "swap" });
 
