29de22de6fa36cecd2b7102e73b5deab606cb92f
magneum-II <yt-dlx@proton.me>
2024-06-29 00:29:57 +0530

    updated

--- middleware.ts
+++ middleware.ts
@@ -4,11 +4,7 @@ import { NextResponse } from "next/server";
 
 export default auth((req) => {
   const isLoggedIn = !!req.auth;
-  if ((req.nextUrl.pathname.startsWith("/cart") || req.nextUrl.pathname.startsWith("/routes") || req.nextUrl.pathname.startsWith("/admin") || req.nextUrl.pathname.startsWith("/api")) && !isLoggedIn) {
-    return NextResponse.redirect(new URL("/", req.nextUrl.origin));
-  }
+  if ((req.nextUrl.pathname.startsWith("/cart") || req.nextUrl.pathname.startsWith("/routes") || req.nextUrl.pathname.startsWith("/admin")) && !isLoggedIn) return NextResponse.redirect(new URL("/", req.nextUrl.origin));
 });
 
-export const config = {
-  matcher: ["/cart/:path*", "/routes/:path*", "/admin/:path*", "/api/:path*"],
-};
+export const config = { matcher: ["/cart/:path*", "/routes/:path*", "/admin/:path*"] };
