e20051076f699c1478a006183be80031b183bb32
magneum-II <yt-dlx@proton.me>
2024-06-29 02:29:34 +0530

    updated

--- app/admin/profile/page.tsx
+++ app/admin/profile/page.tsx
@@ -28,7 +28,15 @@ export default function AdminProfile() {
       <Navbar />
       <div className="mx-auto overflow-hidden bg-[#FFF4E9] max-w-6xl p-2">
         <div className="container mx-auto px-4 py-8">
-          <h1 className="mb-6 text-3xl font-bold text-[#172B25]">Admin Profile</h1>
+          <h1 className="mb-6 text-3xl font-bold text-[#172B25]">Client Profile</h1>
+          <div className="user-info mb-4">
+            <p className="text-lg text-[#172B25]">
+              Name: <span className="font-semibold">{session?.user?.name}</span>
+            </p>
+            <p className="text-lg text-[#172B25]">
+              Email: <span className="font-semibold">{session?.user?.email}</span>
+            </p>
+          </div>
         </div>
       </div>
       <Footer />
