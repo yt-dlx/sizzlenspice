c44afe35f1b3a4af9881fd61b20eeace80a93b34
magneum-II <yt-dlx@proton.me>
2024-07-10 23:35:59 +0530

    updated

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -1,13 +1,7 @@
 // lib/mongodb.ts
 import { MongoClient } from "mongodb";
 
-if (!process.env.MONGODB_URI) {
-  throw new Error('Invalid/Missing environment variable: "MONGODB_URI"');
-}
-
-const uri = process.env.MONGODB_URI;
 const options = {};
-
 let client: MongoClient;
 let clientPromise: Promise<MongoClient>;
 
@@ -15,14 +9,13 @@ if (process.env.NODE_ENV === "development") {
   let globalWithMongo = global as typeof globalThis & {
     _mongoClientPromise?: Promise<MongoClient>;
   };
-
   if (!globalWithMongo._mongoClientPromise) {
-    client = new MongoClient(uri, options);
+    client = new MongoClient(process.env.MONGODB as string, options);
     globalWithMongo._mongoClientPromise = client.connect();
   }
   clientPromise = globalWithMongo._mongoClientPromise;
 } else {
-  client = new MongoClient(uri, options);
+  client = new MongoClient(process.env.MONGODB as string, options);
   clientPromise = client.connect();
 }
 
