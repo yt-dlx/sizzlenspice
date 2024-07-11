8f3f873b3dbbf70584aa5409354f38a1d6adf819
magneum-II <yt-dlx@proton.me>
2024-07-11 23:48:50 +0530

    updated

--- pages/api/websocket.ts
+++ pages/api/websocket.ts
@@ -5,14 +5,11 @@ import type { NextApiResponseServerIO } from "@/types/next";
 
 const SocketHandler = (req: NextApiRequest, res: NextApiResponseServerIO) => {
   if (!res.socket.server.io) {
-    console.log("Socket is initializing");
     const io = new SocketIOServer(res.socket.server as any);
     res.socket.server.io = io;
     io.on("connection", (socket) => {
       socket.on("join-room", (userId) => socket.join(userId));
-      socket.on("update-order", ({ userId, orderId, status }) => {
-        io.to(userId).emit("order-updated", { orderId, status });
-      });
+      socket.on("update-order", ({ userId, orderId, status }) => io.to(userId).emit("order-updated", { orderId, status }));
       socket.on("disconnect", () => {});
     });
   }
