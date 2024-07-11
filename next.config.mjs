4001b719cf7502e4f64ae4f0b8720a607c24dbd2
magneum-II <yt-dlx@proton.me>
2024-07-12 05:13:27 +0530

    updated

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,3 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.resolve.fallback = {
-        ...config.resolve.fallback,
-        fs: false,
-        net: false,
-        tls: false,
-      };
-    }
-    return config;
-  },
-  transpilePackages: ["ably"],
-};
+const nextConfig = {};
 export default nextConfig;
