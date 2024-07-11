63871c2b7b60213fdc05b952316878c4921a2562
magneum-II <yt-dlx@proton.me>
2024-07-11 23:01:19 +0530

    updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,23 +7,14 @@ import Navbar from "./_src/components/Navbar";
 import Footer from "./_src/components/Footer";
 import AnimatedContentWrapper from "./_src/components/AnimatedContentWrapper";
 
-const Kurale = LocalFontLoader({ variable: "--font-Kurale", src: "./_src/fonts/Kurale.ttf" });
-const Playfair = LocalFontLoader({ variable: "--font-Playfair", src: "./_src/fonts/Playfair.ttf" });
-const Grenoble = LocalFontLoader({ variable: "--font-Grenoble", src: "./_src/fonts/Grenoble.ttf" });
-const Brittany = LocalFontLoader({ variable: "--font-Brittany", src: "./_src/fonts/Brittany.otf" });
-const Merriweather = LocalFontLoader({
-  variable: "--font-Merriweather",
-  src: "./_src/fonts/Merriweather.ttf",
-});
-const RobotoCondensed = LocalFontLoader({
-  variable: "--font-RobotoCondensed",
-  src: "./_src/fonts/RobotoCondensed.ttf",
-});
+const Kurale = LocalFontLoader({ variable: "--font-Kurale", src: "../pages/fonts/Kurale.ttf" });
+const Playfair = LocalFontLoader({ variable: "--font-Playfair", src: "../pages/fonts/Playfair.ttf" });
+const Grenoble = LocalFontLoader({ variable: "--font-Grenoble", src: "../pages/fonts/Grenoble.ttf" });
+const Brittany = LocalFontLoader({ variable: "--font-Brittany", src: "../pages/fonts/Brittany.otf" });
+const Merriweather = LocalFontLoader({ variable: "--font-Merriweather", src: "../pages/fonts/Merriweather.ttf" });
+const RobotoCondensed = LocalFontLoader({ variable: "--font-RobotoCondensed", src: "../pages/fonts/RobotoCondensed.ttf" });
 
-export const metadata: Metadata = {
-  description: "Online Food Delivery and Order",
-  title: "Sizzle & Spice",
-};
+export const metadata: Metadata = { description: "Online Food Delivery and Order", title: "Sizzle & Spice" };
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en" className={`h-full ${Kurale.variable} ${Playfair.variable} ${Grenoble.variable} ${Brittany.variable} ${Merriweather.variable} ${RobotoCondensed.variable}`}>
