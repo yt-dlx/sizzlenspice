cb2526fcba3f2edafbc1c3afebcb2fac76dd837b
magneum-II <yt-dlx@proton.me>
2024-07-12 22:19:40 +0530

    updated

--- _backups/ably/app/api/auth/[...nextauth]/route.ts
+++
@@ -1,3 +0,0 @@
-// app/api/auth/[...nextauth]/route.ts
-import { handlers } from "@/auth";
-export const { GET, POST } = handlers;
--- _backups/w-ws/auth.ts
+++
@@ -1,9 +0,0 @@
-// auth.ts
-import NextAuth from "next-auth";
-import GitHub from "next-auth/providers/github";
-import Google from "next-auth/providers/google";
-
-export const { handlers, auth, signIn, signOut } = NextAuth({
-  providers: [GitHub, Google],
-  trustHost: true,
-});
