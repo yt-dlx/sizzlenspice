2e5a77d6e6a552e921b0f461fbd72de44f97a44c
magneum-II <yt-dlx@proton.me>
2024-07-12 15:14:29 +0530

    updated

--- _backups/w-ws/middleware.ts
+++
@@ -1,9 +0,0 @@
-// middleware.ts
-import { auth } from "@/auth";
-import { NextResponse } from "next/server";
-
-export default auth((req) => {
-  const isLoggedIn = !!req.auth;
-  if (!isLoggedIn) if (req.nextUrl.pathname.startsWith("/cart") || req.nextUrl.pathname.startsWith("/home")) return NextResponse.redirect(new URL("/", req.nextUrl.origin));
-});
-export const config = { matcher: ["/home/:path*"] };
