7cdf4a0a394b4a2b379e934bab046231db14f2a2
magneum-II <yt-dlx@proton.me>
2024-07-12 23:50:03 +0530

    updated

--- app/providers.tsx
+++ app/providers.tsx
@@ -2,16 +2,17 @@
 "use client";
 import * as Ably from "ably";
 import { useState } from "react";
-import { AblyProvider } from "ably/react";
 import { AnimatePresence } from "framer-motion";
 import { SessionProvider } from "next-auth/react";
+import { AblyProvider, ChannelProvider } from "ably/react";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ChannelProvider } from "ably/react";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   const [queryClient] = useState(() => new QueryClient());
-  const AblyClient = new Ably.Realtime({ authUrl: "/api/token", authMethod: "POST" });
+  const AblyClient = new Ably.Realtime({ authUrl: "api/token", authMethod: "POST" });
+  AblyClient.connection.on("connected", () => console.log("Ably connected successfully!"));
+  AblyClient.connection.on("disconnected", (err) => console.error("Ably disconnected", err));
 
   return (
     <AblyProvider client={AblyClient}>
