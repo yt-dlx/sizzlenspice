d40bb789d1b69f99b86bb093a8020174bce7290c
magneum-II <yt-dlx@proton.me>
2024-07-14 13:51:11 +0530

    updated

--- _backup/lib/debouce.ts
+++
@@ -1,8 +0,0 @@
-// lib/debounce.ts
-export default function debounce(func: Function, delay: number) {
-  let timer: ReturnType<typeof setTimeout>;
-  return function (this: any, ...args: any[]) {
-    clearTimeout(timer);
-    timer = setTimeout(() => func.apply(this, args), delay);
-  };
-}
