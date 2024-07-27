abdaa0f01ceac84dea1dd46daf6a7250309e34a9
magneum-II <yt-dlx@proton.me>
2024-07-27 19:25:38 +0530

    updated

--- app/routes/restaurant/profile/page.tsx
+++ app/routes/restaurant/profile/page.tsx
@@ -1,5 +1,6 @@
 // app/routes/restaurant/profile/page.tsx
 "use client";
+import Image from "next/image";
 import Loading from "./loading";
 import React, { useEffect } from "react";
 import { useSession } from "next-auth/react";
@@ -109,84 +110,98 @@ const ProfilePage = () => {
   if (isUserLoading || fetchRestaurant.isIdle) return <Loading />;
   if (userError || fetchRestaurant.error) throw userError?.message || fetchRestaurant.error?.message;
   return (
-    <div className="text-secondary flex items-center justify-center text-center max-w-6xl">
-      <h1>Profile Page</h1>
-      <p>Email: {session?.user?.email}</p>
-      <p>Phone Number: {userData?.phoneNumber}</p>
-      <p>Customer Email: {userData?.customerEmail}</p>
-      <p>Location Data:</p>
-      <ul>
-        <li>Latitude: {userData?.locationData.latitude}</li>
-        <li>Longitude: {userData?.locationData.longitude}</li>
-        <li>Address: {userData?.locationData.address}</li>
-        <li>Pincode: {userData?.locationData.pincode}</li>
-      </ul>
+    <main className="max-w-full mx-auto overflow-hidden bg-primary p-4">
+      <section id="header" className="flex flex-col md:justify-center md:items-center sm:text-center text-secondary">
+        <h1 className="text-6xl sm:text-7xl md:text-8xl lg:text-9xl text-secondary">Profile Page</h1>
+        <h2 className="text-lg sm:text-2xl md:text-3xl py-2">Manage Your Restaurant Profile</h2>
+      </section>
       {fetchRestaurant.data && (
-        <div>
-          <h2>Restaurant Data</h2>
-          <p>Name: {fetchRestaurant.data.name}</p>
-          <p>Email: {fetchRestaurant.data.email}</p>
-          <p>Phone Number: {fetchRestaurant.data.phoneNumber}</p>
-          <h3>Categories</h3>
-          <ul>
+        <section id="restaurant-data" className="max-w-2xl sm:max-w-4xl md:max-w-6xl lg:max-w-7xl flex items-center justify-center mx-auto py-4">
+          <div className="gap-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3">
             {fetchRestaurant.data.categories.map((category: any) => (
-              <li key={category.id}>
-                <p>Title: {category.title}</p>
-                <p>Image: {category.image}</p>
-                <button onClick={() => editCategory.mutate({ id: category.id, title: category.title, image: category.image })}>Edit Category</button>
-                <button onClick={() => deleteCategory.mutate(category.id)}>Delete Category</button>
-                <h4>Items</h4>
-                <ul>
-                  {category.items.map((item: any) => (
-                    <li key={item.id}>
-                      <p>Title: {item.title}</p>
-                      <p>Description: {item.description}</p>
-                      <p>Image: {item.image}</p>
-                      <p>Price: {JSON.stringify(item.price)}</p>
-                      <p>Genre: {item.genre}</p>
-                      <p>Rating: {item.rating}</p>
-                      <button
-                        onClick={() =>
-                          editItem.mutate({
-                            id: item.id,
-                            title: item.title,
-                            description: item.description,
-                            image: item.image,
-                            price: item.price,
-                            genre: item.genre,
-                            rating: item.rating,
-                            categoryId: category.id,
-                          })
-                        }
-                      >
-                        Edit Item
-                      </button>
-                      <button onClick={() => deleteItem.mutate(item.id)}>Delete Item</button>
-                    </li>
-                  ))}
-                </ul>
-                <button
-                  onClick={() =>
-                    addItem.mutate({
-                      title: "New Item",
-                      description: "Description",
-                      image: "Image URL",
-                      price: { small: "10", medium: "15", full: "20" },
-                      genre: "Genre",
-                      rating: 4.5,
-                      categoryId: category.id,
-                    })
-                  }
-                >
-                  Add Item
-                </button>
-              </li>
+              <div key={category.id} className="flex flex-col rounded-xl overflow-hidden h-full shadow-md shadow-secondary border-4 border-double border-secondary">
+                <Image width={540} height={540} src={category.image} alt={category.title} className="object-cover w-full h-48" />
+                <div className="text-primary flex flex-col justify-between bg-secondary flex-grow p-4">
+                  <div className="flex justify-between items-center">
+                    <div className="flex items-center space-x-2">
+                      <h2 className="font-bold text-lg">{category.title}</h2>
+                    </div>
+                  </div>
+                  <div className="flex space-x-2 mt-4">
+                    <button
+                      onClick={() => editCategory.mutate({ id: category.id, title: category.title, image: category.image })}
+                      className="text-sm bg-primary hover:bg-tertiary text-secondary p-1 rounded-xl transition duration-300"
+                    >
+                      Edit Category
+                    </button>
+                    <button onClick={() => deleteCategory.mutate(category.id)} className="text-sm bg-red-700 hover:bg-red-800 text-primary p-1 rounded-xl transition duration-300">
+                      Delete Category
+                    </button>
+                  </div>
+                  <h4 className="mt-4">Items</h4>
+                  <ul>
+                    {category.items.map((item: any) => (
+                      <li key={item.id} className="flex flex-col space-y-4">
+                        <p>Title: {item.title}</p>
+                        <p>Description: {item.description}</p>
+                        <p>Image: {item.image}</p>
+                        <p>Price: {JSON.stringify(item.price)}</p>
+                        <p>Genre: {item.genre}</p>
+                        <p>Rating: {item.rating}</p>
+                        <div className="flex space-x-2">
+                          <button
+                            onClick={() =>
+                              editItem.mutate({
+                                id: item.id,
+                                title: item.title,
+                                description: item.description,
+                                image: item.image,
+                                price: item.price,
+                                genre: item.genre,
+                                rating: item.rating,
+                                categoryId: category.id,
+                              })
+                            }
+                            className="text-sm bg-primary hover:bg-tertiary text-secondary p-1 rounded-xl transition duration-300"
+                          >
+                            Edit Item
+                          </button>
+                          <button onClick={() => deleteItem.mutate(item.id)} className="text-sm bg-red-700 hover:bg-red-800 text-primary p-1 rounded-xl transition duration-300">
+                            Delete Item
+                          </button>
+                        </div>
+                      </li>
+                    ))}
+                  </ul>
+                  <button
+                    onClick={() =>
+                      addItem.mutate({
+                        title: "New Item",
+                        description: "Description",
+                        image: "Image URL",
+                        price: { small: "10", medium: "15", full: "20" },
+                        genre: "Genre",
+                        rating: 4.5,
+                        categoryId: category.id,
+                      })
+                    }
+                    className="text-sm bg-primary hover:bg-tertiary text-secondary p-1 rounded-xl transition duration-300"
+                  >
+                    Add Item
+                  </button>
+                </div>
+              </div>
             ))}
-          </ul>
-          <button onClick={() => addCategory.mutate({ title: "New Category", image: "Image URL" })}>Add Category</button>
-        </div>
+          </div>
+          <button
+            onClick={() => addCategory.mutate({ title: "New Category", image: "Image URL" })}
+            className="text-sm bg-primary hover:bg-tertiary text-secondary p-1 rounded-xl transition duration-300 mt-4"
+          >
+            Add Category
+          </button>
+        </section>
       )}
-    </div>
+    </main>
   );
 };
 
