53110e6ce4ed1e1e75f74d3c8700764ffda57de6
magneum-II <yt-dlx@proton.me>
2024-07-30 12:38:37 +0530

    updated

--- app/routes/restaurant/register/page.tsx
+++ app/routes/restaurant/register/page.tsx
@@ -1,25 +1,31 @@
 // app/routes/restaurant/register/page.tsx
 "use client";
-import { useState, FormEvent } from "react";
-import { useRouter } from "next/navigation";
 import Loading from "@/app/routes/loading";
+import { useRouter } from "next/navigation";
+import { useState, FormEvent } from "react";
+import { useMutation } from "@tanstack/react-query";
 
 export default function RegisterPage() {
   const router = useRouter();
-  const [userData, setUserData] = useState({ email: "", phoneNumber: "", customerEmail: "" });
   const [loading, setLoading] = useState(false);
+  const [userData, setUserData] = useState({ email: "", phoneNumber: "", name: "" });
   const handleInputChange = (field: string, value: string) => setUserData((prev) => ({ ...prev, [field]: value }));
+  const registerMutation = useMutation({
+    mutationFn: async (data: { email: string; phoneNumber: string; name: string }) => {
+      const response = await fetch("/api/restaurant/register", { method: "POST", body: JSON.stringify(data), headers: { "Content-Type": "application/json" } });
+      if (!response.ok) throw new Error("Network response was not ok");
+      return response.json();
+    },
+    onError: (error) => console.error("Failed to register user", error),
+    onSuccess: () => router.push("/routes/restaurant/auth"),
+    onSettled: () => setLoading(false),
+  });
   const handleRegister = async (event: FormEvent) => {
     event.preventDefault();
     setLoading(true);
-    const response = await fetch("/api/restaurant/register", { method: "POST", headers: { "Content-Type": "application/json" }, body: JSON.stringify(userData) });
-    if (response.ok) router.push("/routes/restaurant/auth");
-    else console.error("Failed to register user");
-
-    setLoading(false);
+    registerMutation.mutate(userData);
   };
   if (loading) return <Loading />;
-
   return (
     <main className="max-w-full mx-auto overflow-hidden bg-primary p-4">
       <section id="header" className="flex flex-col md:justify-center md:items-center sm:text-center text-secondary">
@@ -30,32 +36,33 @@ export default function RegisterPage() {
           <div className="grid grid-cols-1 md:grid-cols-2 gap-2 w-full mb-8">
             <div className="relative flex-grow">
               <input
+                required
+                readOnly
                 type="email"
+                placeholder="Email"
                 value={userData.email}
                 onChange={(e) => handleInputChange("email", e.target.value)}
-                placeholder="Email"
                 className="w-full py-2 pl-10 pr-4 rounded-xl bg-primary border-2 border-secondary shadow-md shadow-secondary text-secondary placeholder-secondary focus:border-primary focus:ring-primary"
-                required
               />
             </div>
             <div className="relative flex-grow">
               <input
                 type="tel"
+                required
+                placeholder="Phone Number"
                 value={userData.phoneNumber}
                 onChange={(e) => handleInputChange("phoneNumber", e.target.value)}
-                placeholder="Phone Number"
                 className="w-full py-2 pl-10 pr-4 rounded-xl bg-primary border-2 border-secondary shadow-md shadow-secondary text-secondary placeholder-secondary focus:border-primary focus:ring-primary"
-                required
               />
             </div>
             <div className="relative flex-grow">
               <input
-                type="email"
-                value={userData.customerEmail}
-                onChange={(e) => handleInputChange("customerEmail", e.target.value)}
-                placeholder="Customer Email"
-                className="w-full py-2 pl-10 pr-4 rounded-xl bg-primary border-2 border-secondary shadow-md shadow-secondary text-secondary placeholder-secondary focus:border-primary focus:ring-primary"
                 required
+                type="text"
+                placeholder="Name"
+                value={userData.name}
+                onChange={(e) => handleInputChange("name", e.target.value)}
+                className="w-full py-2 pl-10 pr-4 rounded-xl bg-primary border-2 border-secondary shadow-md shadow-secondary text-secondary placeholder-secondary focus:border-primary focus:ring-primary"
               />
             </div>
           </div>
